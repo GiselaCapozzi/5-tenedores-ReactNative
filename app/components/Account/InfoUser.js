ab86a77320b9daa0abd0c3c2c08d0c47ff519b0a
GiselaCapozzi <gisecapozzi@gmail.com>
2021-10-14 13:32:19 -0300

    InfoUserAvatar

--- app/components/Account/InfoUser.js
+++ app/components/Account/InfoUser.js
@@ -1,16 +1,50 @@
 import React from "react";
 import { StyleSheet, View, Text } from 'react-native';
+import { Avatar, Accessory } from 'react-native-elements';
 
-const InfoUser = () => {
+const InfoUser = (props) => {
+  const { userInfo } = props;
+  console.log(userInfo);
+
   return (
-    <View>
-      <Text>INFO USER...</Text>
+    <View style={styles.viewUserInfo}>
+      <Avatar 
+        rounded
+        size="large"
+        showEditButton
+        containerStyle={styles.userInfoAvatar}
+      >
+        <Avatar.Accessory 
+          size={30}
+        />
+      </Avatar>
+      <View>
+        <Text style={styles.displayName}>
+          Gisela Capozzi
+        </Text>
+        <Text>gise@gmail.com</Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-
+  viewUserInfo: {
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    backgroundColor: "#f2f2f2",
+    paddingTop: 30,
+    paddingBottom: 30
+  },
+  userInfoAvatar: {
+    marginRight: 20,
+    backgroundColor: "grey"
+  },
+  displayName: {
+    fontWeight: "bold",
+    paddingBottom: 5
+  }
 })
 
 export default InfoUser;
