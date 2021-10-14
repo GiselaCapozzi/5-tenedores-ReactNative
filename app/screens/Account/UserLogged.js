ddf7458935e4a0f6541b04c92a6cfb94e5fc82fc
GiselaCapozzi <gisecapozzi@gmail.com>
2021-10-14 12:03:24 -0300

    first commit

--- app/screens/Account/UserLogged.js
+++ app/screens/Account/UserLogged.js
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, Text } from 'react-native';
-import {  } from 'react-native-element';
+import { Button } from 'react-native-elements';
+import FlashMessage from "react-native-flash-message";
 import firebase from "firebase";
 
+import Loading from '../../components/Loading';
+
 const UserLogged = () => {
+  const [loading, setLoading] = useState(false);
+  const [loadingText, setLoadingText] = useState('');
+
   return (
-    <View>
-      <Text>
-        UserLogged...
-      </Text>
+    <View style={styles.viewUserInfo}>
+      <Text>InfoUser...</Text>
+      <Text>AccountOptions</Text>
       <Button 
         title="Cerrar sesión"
-        onPress = {() => firebase.auth().signOut()}
+        buttonStyle={styles.btnCloseSession}
+        titleStyle={styles.btnCloseSessionText}
+        onPress={() => firebase.auth().signOut()}
       />
+      <FlashMessage position="center" />
+      <Loading text={loadingText} isVisible={loading}/>
     </View>
   )
 };
 
 const styles = StyleSheet.create({
-
+  viewUserInfo: {
+    minHeight: "100%",
+    backgroundColor: "#f2f2f2"
+  },
+  btnCloseSession: {
+    marginTop: 30,
+    borderRadius: 0,
+    backgroundColor: "#fff",
+    borderTopWidth: 1,
+    borderTopColor: "#e3e3e3",
+    borderBottomWidth: 1,
+    borderBottomColor: "#e3e3e3",
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  btnCloseSessionText: {
+   color: "#00a680"
+  }
 })
 
 export default UserLogged;
