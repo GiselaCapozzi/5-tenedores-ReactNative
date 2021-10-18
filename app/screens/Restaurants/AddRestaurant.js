5ab35189f3fd0b70e8a4c708336471a9077af4d2
GiselaCapozzi <gisecapozzi@gmail.com>
2021-10-18 17:41:00 -0300

    Formulario de alta de restaurantes

--- app/screens/Restaurants/AddRestaurant.js
+++ app/screens/Restaurants/AddRestaurant.js
@@ -1,18 +1,27 @@
-import React from 'react'
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState, useRef } from 'react'
+import { View } from 'react-native';
+import FlashMessage from 'react-native-flash-message';
 
-const AddRestaurant = () => {
+import Loading from '../../components/Loading';
+import AddRestaurantForm from '../../components/Restaurants/AddRestaurantForm';
+
+const AddRestaurant = (props) => {
+  const { navigation } = props;
+
+  const [isLoading, setIsLoading] = useState(false);
+  const flashRef = useRef();
+
   return (
     <View>
-      <Text>
-        AddRestaurant...
-      </Text>
+      <AddRestaurantForm
+        flashRef={flashRef}
+        setIsLoading={setIsLoading}
+        navigation={navigation}
+      />
+      <FlashMessage option='center' />
+      <Loading isVisible={isLoading} text='Creando restaurante' />
     </View>
   )
 };
-
-const styles = StyleSheet.create({
-
-});
 
 export default AddRestaurant;
