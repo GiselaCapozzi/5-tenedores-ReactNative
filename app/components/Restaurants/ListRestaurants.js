06a6b495b5961b0de5c5cc4a50a949ddafb49bc7
GiselaCapozzi <gisecapozzi@gmail.com>
2021-10-21 15:49:07 -0300

    Mostrando la información de cada restaurante

--- app/components/Restaurants/ListRestaurants.js
+++ app/components/Restaurants/ListRestaurants.js
@@ -34,12 +34,38 @@ const ListRestaurants = (props) => {
 }
 
 const Restaurant = (props) => {
-  const { restaurant } = props
+  const { restaurant } = props;
+  const { images, name, description, address } = restaurant.item;
+  const imageRestaurant = images[0];
+
+  const goRestaurant = () => {
+    console.log('OK!');
+  }
 
   return (
-    <View>
-      <Text>Restaurante</Text>
-    </View>
+    <TouchableOpacity onPress={goRestaurant}>
+      <View style={styles.viewRestaurant}>
+        <View style={styles.viewRestaurantImage}>
+          <Image
+            resizeMode='cover'
+            PlaceholderContent={<ActivityIndicator size='small' color="#000" />}
+            source={
+              imageRestaurant ?
+                { uri: imageRestaurant } :
+                require('../../../assets/img/no-image.png')
+            }
+            style={styles.imageRestaurant}
+          />
+        </View>
+        <View>
+          <Text style={styles.restaurantName}>{name}</Text>
+          <Text style={styles.restaurantAddress}>{address}</Text>
+          <Text style={styles.restaurantDescription}>
+            {description.substr(0, 60)}...
+          </Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   )
 }
 
@@ -48,6 +74,29 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 10,
     alignItems: 'center'
+  },
+  viewRestaurant: {
+    flexDirection: 'row',
+    margin: 10
+  },
+  viewRestaurantImage: {
+    marginRight: 15
+  },
+  imageRestaurant: {
+    width: 80,
+    height: 80
+  },
+  restaurantName: {
+    fontWeight: 'bold'
+  },
+  restaurantAddress: {
+    paddingTop: 2,
+    color: 'grey'
+  },
+  restaurantDescription: {
+    paddingTop: 2,
+    color: 'grey',
+    width: 300
   }
 });
 
