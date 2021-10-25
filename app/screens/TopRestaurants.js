9820cd36fc07367f830cc2b60fe278d7c6235597
GiselaCapozzi <gisecapozzi@gmail.com>
2021-10-25 12:56:41 -0300

    Obteniendo los 5 restaurantes mejor puntuados

--- app/screens/TopRestaurants.js
+++ app/screens/TopRestaurants.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text }  from 'react-native';
+import FlashMessage, { showMessage } from 'react-native-flash-message';
 
-const TopRestaurants = () => {
+import { firebaseApp } from '../utils/firebase';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const db = firebase.firestore(firebaseApp);
+
+const TopRestaurants = (props) => {
+  const { navigation } = props;
+  const [restaurants, setRestaurants] = useState([]);
+
+  console.log(restaurants);
+
+  useEffect(() => {
+    db.collection('restaurants')
+    .orderBy('rating', 'desc')
+    .limit(5)
+    .get()
+    .then((response) => {
+      const restaurantArray = [];
+      response.forEach((doc) => {
+        const data = doc.data();
+        data.id = doc.data();
+        restaurantArray.push(data);
+      });
+      setRestaurants(restaurantArray);
+    })
+  }, [])
+
   return (
     <View>
       <Text>
         TopRestaurants...
       </Text>
+      <FlashMessage position='center'/>
     </View>
   )
 };
