db2b4f68426862bc9505a044d11125720727815e
GiselaCapozzi <gisecapozzi@gmail.com>
2021-10-25 11:57:05 -0300

    Diseñando los restaurantes

--- app/screens/Favorites.js
+++ app/screens/Favorites.js
@@ -156,10 +156,36 @@ const UserNoLogged = (props) => {
 
 const Restaurant = (props) => {
   const { restaurant } = props;
-  const { name } = restaurant.item;
+  const { name, images } = restaurant.item;
+  
   return (
-    <View>
-      <Text>{name}</Text>
+    <View style={styles.restaurant}>
+      <TouchableOpacity 
+        onPress={() => console.log('IR')}
+      />
+      <Image 
+        resizeMode='cover'
+        style={styles.image}
+        PlaceholderContent={<ActivityIndicator 
+          color='#fff'
+        />}
+        source={
+          images[0]
+          ? {uri: images[0]} :
+          require('../../assets/img/no-image.png')
+        }
+      />
+      <View style={styles.info}>
+        <Text style={styles.name}>{name}</Text>
+        <Icon 
+          type='material-community'
+          name='heart'
+          color='#f00'
+          containerStyle={styles.favorite}
+          onPress={() => console.log('Remove')}
+          underlayColor='transparent'
+        />
+      </View>
     </View>
   )
 }
@@ -175,6 +201,35 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginTop: 10,
     marginBottom: 10
+  },
+  restaurant: {
+    margin: 10
+  },
+  image: {
+    width: '100%',
+    height: 180
+  },
+  info: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginTop: -30,
+    backgroundColor: '#fff'
+  },
+  name: {
+    fontWeight: 'bold',
+    fontSize: 15
+  },
+  favorite: {
+    marginTop: -35,
+    backgroundColor: '#fff',
+    padding: 15,
+    borderRadius: 100
   }
 });
 
